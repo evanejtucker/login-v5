127f453e0718cf1607b7b42c5ccd935a57e52214
Evan Tucker <eej.tucker@gmail.com>
2018-01-24 16:05:15 -0700

    worked on file structure and server

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+// dependencies
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const main = require('./app/routes/main.js');
+const profile = require('./app/routes/profile.js');
+const mongoose = require('mongoose');
+
+// middleware
+app.set('view engine', 'ejs');
+app.use(express.static('./app/public'));
+
+// mongoose
+mongoose.connect('mongodb://localhost/login-v5');
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("successfully connected to db");
+});
+
+// routes
+app.use('/', main);
+app.use('/profile', profile);
 
 app.listen(port, ()=> {
     console.log(`app listening on port ${port}!`)
