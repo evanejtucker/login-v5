c6a148529e8c84fb704aff72c0e3e3288a6516fb
Evan Tucker <eej.tucker@gmail.com>
2018-01-25 09:09:10 -0700

    fixed partials issue

--- app/routes/main.js
+++ app/routes/main.js
@@ -5,10 +5,10 @@ var router = express.Router()
 // define the home page route
 router.get('/', function (req, res) {
   res.render('pages/index.ejs', {name: 'Evan'});
-})
+});
 // define the about route
 router.get('/about', function (req, res) {
   res.send('About page')
-})
+});
 
 module.exports = router
